112fc8bf8f987fc87a62a6b1e413e380678d5cfd
sunitha <sunithawisptools@gmail.com>
2017-04-27 13:48:52 +0530

    newchange

--- fileinbr1.php
+++ fileinbr1.php
@@ -1,5 +1,6 @@
 <?php
   echo " I am in br1 branch and need to be in master branch";
+  echo "ABCD";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
